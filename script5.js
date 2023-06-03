791a8e36211d8c32a94eb505c0c143907dcefd46
Rufus602 <dauren_2003@bk.ru>
2023-06-03 12:58:10 +0600

    first commit

--- script5.js
+++
@@ -1,8 +0,0 @@
-let arr = prompt("Введите слово:");
-let result = "";
-for (let i = arr.length - 1; i >= 0; i--) {
-  result += arr[i];
-}
-alert(result);
-console.log(result);
-document.write(result);
